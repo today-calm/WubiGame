e1bf2fc44cb9f7f4518071fa98e84bb206eb0bd9
today-calm <todaycalm@protonmail.com>
2023-07-21 22:34:59 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-publicPath:'./'
-}
+  // 部署 GitHub Pages
+  // https://cli.vuejs.org/zh/guide/deployment.html#github-pages
+
+  // publicPath 如何配置
+  // https://segmentfault.com/a/1190000042107272
+
+  publicPath: './'
+};
